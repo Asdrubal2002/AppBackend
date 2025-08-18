ccc25585febb662a860569f756aca5d9ac12c886
Asdrubal <henryasdrubalrodriguezmorales@gmail.com>
2025-08-18 13:02:48 -0500

    Actualizacion

--- marketplace/firebase_config.py
+++ marketplace/firebase_config.py
@@ -1,15 +1,15 @@
+# marketplace/firebase_config.py
 import os
-import json
 from firebase_admin import credentials, initialize_app
+from django.conf import settings
 
-firebase_config = os.getenv("FIREBASE_CONFIG")
+# Intenta usar el archivo secreto en Render
+cred_path = "/etc/secrets/firebasedocument.json"
 
-if firebase_config:
-    cred_dict = json.loads(firebase_config)
-    cred = credentials.Certificate(cred_dict)
-else:
-    from django.conf import settings
+if not os.path.exists(cred_path):
+    # Si no existe (ej: entorno local), usa el archivo normal
     cred_path = os.path.join(settings.BASE_DIR, 'marketplace', 'firebasedocument.json')
-    cred = credentials.Certificate(cred_path)
 
+cred = credentials.Certificate(cred_path)
 firebase_app = initialize_app(cred)
+
