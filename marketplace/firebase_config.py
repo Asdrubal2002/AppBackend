07042f9b0dc974c4f6d6ef40f62bcab5f8e660b8
Asdrubal <henryasdrubalrodriguezmorales@gmail.com>
2025-08-18 12:53:00 -0500

    Actualizacion

--- marketplace/firebase_config.py
+++ marketplace/firebase_config.py
@@ -1,8 +1,15 @@
-# marketplace/firebase_config.py
 import os
+import json
 from firebase_admin import credentials, initialize_app
-from django.conf import settings
 
-cred_path = os.path.join(settings.BASE_DIR, 'marketplace', 'firebasedocument.json')
-cred = credentials.Certificate(cred_path)
-firebase_app = initialize_app(cred)  # Renombrado como firebase_app por claridad
+firebase_config = os.getenv("FIREBASE_CONFIG")
+
+if firebase_config:
+    cred_dict = json.loads(firebase_config)
+    cred = credentials.Certificate(cred_dict)
+else:
+    from django.conf import settings
+    cred_path = os.path.join(settings.BASE_DIR, 'marketplace', 'firebasedocument.json')
+    cred = credentials.Certificate(cred_path)
+
+firebase_app = initialize_app(cred)
